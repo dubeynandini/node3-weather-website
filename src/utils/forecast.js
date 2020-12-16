9a362a4f7a6709c216571a1f95f69c88ce2c1933
Nandini Dubey <dubeynandini73@gmail.com>
2020-12-17 02:18:17 +0530

    Add info to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0]+ '. It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + ' degrees though.')
+            callback(undefined, body.current.weather_descriptions[0]+ '. It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + ' degrees. It is ' + body.current.humidity + '% humid there.')
         }
     })
 }
